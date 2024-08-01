e25930798ce91bc85ad65d675437c36e7caf42dc
vipul-rawat <vipul.rawat@gofr.dev>
2024-08-01 13:22:11 +0530

    add init project sub command

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"gofr.dev/gofr-cli/project"
 	"gofr.dev/pkg/gofr"
 )
 
@@ -10,6 +11,8 @@ func main() {
 	cli.SubCommand("version", func(*gofr.Context) (interface{}, error) {
 		return CLIVersion, nil
 	})
+
+	cli.SubCommand("init", project.Create)
 
 	cli.Run()
 }
