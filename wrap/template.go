2b778de319cacb97cfbbc9a3377776331e473516
Gajendra Malviya <gajendra.malviya@zop.dev>
2026-07-29 15:18:45 +0530

    Revert "Fix generated server not compiling (request wrapper name + stream-only imports)"
    This reverts commit 26522fc92d49a91bf02cd36d8272571a7e0656cc.

--- wrap/template.go
+++ wrap/template.go
@@ -14,33 +14,22 @@ const (
     {{- end }}
 {{- end }}
 
-{{- $hasStream := false }}
-{{- range .Methods }}
-    {{- if or .StreamsRequest .StreamsResponse }}
-        {{- $hasStream = true }}
-    {{- end }}
-{{- end }}
-
 package {{ .Package }}
 
 import (
 	"context"
-	{{- if $hasStream }}
 	"time"
-	{{- end }}
-
+	
 	"gofr.dev/pkg/gofr"
 	"gofr.dev/pkg/gofr/container"
-	{{- if $hasStream }}
 	gofrgRPC "gofr.dev/pkg/gofr/grpc"
-	{{- end }}
 	"google.golang.org/grpc"
 
 	{{- if $hasUnary }}
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	{{- end }}
-
+	
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
@@ -69,6 +58,13 @@ type {{ .Service }}ServiceWrapper struct {
 	Container *container.Container
 	server    {{ .Service }}ServiceWithGofr
 }
+
+{{- $hasStream := false }}
+{{- range .Methods }}
+    {{- if or .StreamsRequest .StreamsResponse }}
+        {{- $hasStream = true }}
+    {{- end }}
+{{- end }}
 
 {{- if $hasStream }}
 // Base instrumented stream
@@ -332,30 +328,30 @@ import (
 
 // Request Wrappers
 {{- range $request := .Requests }}
-type {{ $request.Request }}Wrapper struct {
+type {{ $request }}Wrapper struct {
 	ctx context.Context
-	*{{ $request.Request }}
+	*{{ $request }}
 }
 
-func (h *{{ $request.Request }}Wrapper) Context() context.Context {
+func (h *{{ $request }}Wrapper) Context() context.Context {
 	return h.ctx
 }
 
-func (h *{{ $request.Request }}Wrapper) Param(s string) string {
+func (h *{{ $request }}Wrapper) Param(s string) string {
 	return ""
 }
 
-func (h *{{ $request.Request }}Wrapper) PathParam(s string) string {
+func (h *{{ $request }}Wrapper) PathParam(s string) string {
 	return ""
 }
 
-func (h *{{ $request.Request }}Wrapper) Bind(p interface{}) error {
+func (h *{{ $request }}Wrapper) Bind(p interface{}) error {
 	ptr := reflect.ValueOf(p)
 	if ptr.Kind() != reflect.Ptr {
 		return fmt.Errorf("expected a pointer, got %T", p)
 	}
 
-	hValue := reflect.ValueOf(h.{{ $request.Request }}).Elem()
+	hValue := reflect.ValueOf(h.{{ $request }}).Elem()
 	ptrValue := ptr.Elem()
 
 	for i := 0; i < hValue.NumField(); i++ {
@@ -372,11 +368,11 @@ func (h *{{ $request.Request }}Wrapper) Bind(p interface{}) error {
 	return nil
 }
 
-func (h *{{ $request.Request }}Wrapper) HostName() string {
+func (h *{{ $request }}Wrapper) HostName() string {
 	return ""
 }
 
-func (h *{{ $request.Request }}Wrapper) Params(s string) []string {
+func (h *{{ $request }}Wrapper) Params(s string) []string {
 	return nil
 }
 {{- end }}`
